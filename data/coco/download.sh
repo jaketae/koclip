6a55791d8ffe982d42c1402fb55ba484ccc9cf71
Jake Tae <jaesungtae@gmail.com>
2021-07-17 08:43:51 +0000

    chore: add zipped coco ko annotations

--- data/coco/download.sh
+++
@@ -1,4 +0,0 @@
-wget http://images.cocodataset.org/zips/train2014.zip
-wget http://images.cocodataset.org/zips/val2014.zip
-unzip train2014.zip
-unzip val2014.zip
---
+++ data/coco/prep.sh
@@ -0,0 +1,5 @@
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
+unzip train2014.zip
+unzip val2014.zip
+unzip annotations.zip
